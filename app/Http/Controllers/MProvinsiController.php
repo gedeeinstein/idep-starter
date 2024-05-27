508fb75d15e3f6627e75286037bc92871d05506a
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-05-27 17:19:04 +0800

    Creating Model Based MVC for all related mProvinsi using command php artisan make:model mProvinsi --all

---
+++ app/Http/Controllers/MProvinsiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mProvinsi;
+use App\Http\Requests\StoremProvinsiRequest;
+use App\Http\Requests\UpdatemProvinsiRequest;
+
+class MProvinsiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoremProvinsiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(mProvinsi $mProvinsi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(mProvinsi $mProvinsi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatemProvinsiRequest $request, mProvinsi $mProvinsi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(mProvinsi $mProvinsi)
+    {
+        //
+    }
+}
